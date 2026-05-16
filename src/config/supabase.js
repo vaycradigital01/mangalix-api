af70077324e87bde22a360853ff0afc1fa89cd75
vaycradigital01 <vaycradigital@gmail.com>
2026-05-16 19:23:31 -0300

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,9 +1,13 @@
 const { createClient } = require('@supabase/supabase-js');
+const ws = require('ws');
 
 const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY,
-  { auth: { autoRefreshToken: false, persistSession: false } }
+  {
+    auth: { autoRefreshToken: false, persistSession: false },
+    realtime: { transport: ws }
+  }
 );
 
 module.exports = { supabaseAdmin };
